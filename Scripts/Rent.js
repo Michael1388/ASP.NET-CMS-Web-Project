97490d1a8112c7a81485668c278f93bb9c80a8f0
Michael1388 <119345695+Michael1388@users.noreply.github.com>
2023-05-05 13:42:45 -0500

    Theatre Vertigo Project
    ASP.NET MVC Theatre Vertigo CMS Project

---
+++ Scripts/Rent.js
@@ -0,0 +1,8 @@
+﻿$('#RentalDamaged').click(function () {
+
+    if ($('#RentalDamaged').is(':checked')) {
+        $('.rental_history-create--damagesincurred').html('Damages Incurred');
+    } else {
+        $('.rental_history-create--damagesincurred').html('Notes');
+    }
+});
